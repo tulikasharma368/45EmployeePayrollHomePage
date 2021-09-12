d77463e21099defd34a213614dc3d77f5b7fb088
Tulikasharma <tulikasharma368@gmail.com>
2021-09-13 01:14:05 +0530

    Display employee details from local storage

--- js/new-home.js
+++ js/new-home.js
@@ -1,13 +1,22 @@
+let empPayrollList;
 window.addEventListener('DOMContentLoaded', (event) => {
+    empPayrollList = getEmployeePayrollDateFromLocalStorage();
+    document.querySelector(".emp-count").textContent = empPayrollList.length;
     createInnerHtml();
 });
+
+//UC 6
+const getEmployeePayrollDateFromLocalStorage = () => {
+    return localStorage.getItem('EmployeePayrollList')? JSON.parse(localStorage.getItem('EmployeePayrollList')) :[];
+}
+
+
 //UC4
 //template literal ES6
 const createInnerHtml = () => {
     const headerHtml = "  <th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th>";
-
+    if(empPayrollList.length == 0) return;
     let innerHtml = `${headerHtml}`;
-    let empPayrollList = createEmployeePayrollJSON();
     for (const empPayrollData of empPayrollList){
         innerHtml = `${innerHtml}
             <tr>
@@ -34,50 +43,6 @@ const createInnerHtml = () => {
     document.querySelector('#table-display').innerHTML = innerHtml;
 }
 
-const createEmployeePayrollJSON = () =>{
-    let employeePayrollListLocal = [
-        {
-            _name: 'Chandler Bing',
-            _gender: 'Male',
-            _department: [
-                'Engineer',
-                'Finance'
-            ],
-            _salary: '500000',
-            _startDate: '02 Aug 2021',
-            _note: '',
-            _id: new Date().getTime(),
-            _profilePic: '/assets/profile-images/Ellipse -3.png'
-        },
-        {
-            _name: 'Rachel Green',
-            _gender: 'Female',
-            _department: [
-                'HR', 
-                'Sales'
-            ],
-            _salary: '600000',
-            _startDate: '20 July 2021',
-            _note: '',
-            _id: new Date().getTime(),
-            _profilePic: '/assets/profile-images/Ellipse -1.png'
-        },
-        {
-            _name: 'Joey Tribuani',
-            _gender: 'Male',
-            _department: [
-                'Other' 
-            ],
-            _salary: '700000',
-            _startDate: '01 Oct 2021',
-            _note: '',
-            _id: new Date().getTime(),
-            _profilePic: '/assets/profile-images/Ellipse -8.png'
-        }
-    ];
-    return employeePayrollListLocal;
-}
-
 const getDeptHtml = (deptList) => {
     let deptHtml = '';
     for(const dept of deptList){
@@ -85,3 +50,4 @@ const getDeptHtml = (deptList) => {
     }
     return deptHtml;
 }
+
